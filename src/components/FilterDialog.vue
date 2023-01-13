8a083f65ad478bdc58c46918f74953ace0521631
knhmb <khaled@marvelconnect.com>
2023-01-13 16:24:42 +0800

    Modifications

--- src/components/FilterDialog.vue
+++ src/components/FilterDialog.vue
@@ -3,17 +3,30 @@
     <el-dialog @close="closeDialog" :model-value="filterDialog" width="55%">
       <div class="header">
         <h3>Filter</h3>
-        <img src="../assets/close.png" alt="" />
+        <img @click="closeDialog" src="../assets/close.png" alt="" />
       </div>
-
-      <GoogleMap
-        api-key="AIzaSyA_C47k8nUTryyy5VSf-ddIIKVPyLrQ0R4"
-        style="width: 100%; height: 500px"
-        :center="center"
-        :zoom="15"
-      >
-        <Marker :options="{ position: center }" />
-      </GoogleMap>
+      <div class="map-content">
+        <GoogleMap
+          api-key="AIzaSyA_C47k8nUTryyy5VSf-ddIIKVPyLrQ0R4"
+          style="width: 100%; height: 500px"
+          :center="center"
+          :zoom="15"
+        >
+          <Marker :options="{ position: center }" />
+        </GoogleMap>
+      </div>
+      <el-form-item label="Restaurant Distance">
+        <el-input v-model="restaurantDistance"></el-input>
+      </el-form-item>
+      <el-form-item label="Restaurant Type">
+        <el-input
+          placeholder="Select Restaurant Type"
+          v-model="restaurantType"
+        ></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button>Apply</el-button>
+      </el-form-item>
     </el-dialog>
   </div>
 </template>
@@ -33,6 +46,8 @@ export default {
         lat: 10,
         lng: 10,
       },
+      restaurantDistance: "",
+      restaurantType: "",
     };
   },
   methods: {
@@ -55,6 +70,10 @@ export default {
 </script>
 
 <style scoped>
+.filter-dialog :deep(.el-dialog) {
+  border-radius: 8px;
+}
+
 .filter-dialog :deep(.el-dialog__header) {
   display: none;
 }
@@ -78,5 +97,50 @@ export default {
 
 .filter-dialog .header img {
   width: 1rem;
+  cursor: pointer;
+}
+
+.filter-dialog .map-content {
+  margin-bottom: 1rem;
+}
+
+.filter-dialog :deep(.el-form-item.asterisk-left) {
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.filter-dialog :deep(.el-form-item__content) {
+  width: 100%;
+}
+
+.filter-dialog :deep(.el-form-item__label) {
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  /* line-height: 20px; */
+  color: rgba(20, 20, 20, 0.6);
+}
+
+.filter-dialog .el-button {
+  background: #fe5d1f;
+  border-radius: 8px;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+  letter-spacing: -0.02em;
+  color: #ffffff;
+  width: 100%;
+  padding: 1.3rem;
+  border-color: #fe5d1f;
+}
+
+.filter-dialog :deep(.el-input__wrapper) {
+  background: #f5f5f5;
+  /* border: 1px solid #ebebeb; */
+  border-radius: 8px;
+  padding: 0.3rem 1rem;
 }
 </style>
